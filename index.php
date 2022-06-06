bc60fdf864ecd05645e1fed0765cad2123f721e0
YAMALAPALLI SOWMYA <57365617+sowmya421@users.noreply.github.com>
2022-06-06 16:15:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@personalportfolio.com';
+$email_from = 'info@personelp.herokuapp.com';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
